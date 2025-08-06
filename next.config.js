2b08f5e9b1fac6bdc8b490740f79b113054111fb
punamzagade <98390383+punamzagade@users.noreply.github.com>
2025-08-06 17:10:42 +0530

    redirct

--- next.config.js
+++ next.config.js
@@ -27,24 +27,46 @@
 
 // module.exports = nextConfig;
 
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   reactStrictMode: false,
+//   // output: 'export',
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'api.servicecnnekt.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+//   // ❌ Remove rewrites — not supported with 'output: export'
+// };
+
+// module.exports = nextConfig;
+
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: false,
-  // output: 'export',
+  output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.servicecnnekt.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
-  // ❌ Remove rewrites — not supported with 'output: export'
+  rewrites: async () => {
+    return [
+      {
+        source: "/basepath/:path*",
+        destination: "https://api.servicecnnekt.com/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
